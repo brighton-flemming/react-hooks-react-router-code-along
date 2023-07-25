86dd433a23339f3707a48f3471a5b0b28971fd10
brighton.flemming <brighton.flemming@student.moringaschool.com>
2023-07-25 14:48:33 +0300

    Mission Passed

--- src/index.js
+++ src/index.js
@@ -47,6 +47,8 @@ ReactDOM.render(
   <BrowserRouter>
   <Routes>
     <Route path="/"element={<Home />} />
+    <Route path="/about"element={<About />} />
+    <Route path="/login"element={<Login />} />
   </Routes>
   </BrowserRouter>,
   document.getElementById("root")
