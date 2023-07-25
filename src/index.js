a1fc67ad8c26a51b196fbcd9bf2f6efef2f3f0ed
brighton.flemming <brighton.flemming@student.moringaschool.com>
2023-07-25 14:57:44 +0300

    Mission Passed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+
+const linkStyles = {
+  display: "inline-block",
+  width: "50px",
+  padding: "12px",
+  margin:"0 6px 6px",
+  background: "blue",
+  textDecoration: "none",
+  color: "blue",
+};
 
 function Home() {
   return (
